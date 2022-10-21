6d823eeb298555a1c90df4ffde840561202ff5d7
unknown <natapon1996@gmail.com>
2022-10-21 16:07:16 +0700

    resistor-color python solution

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,18 @@
+# get color value
+def color_code(color):
+    return colors().index(color)
+
+# get color list
+def colors():
+    return [
+            "black",
+            "brown",
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "violet",
+            "grey",
+            "white",
+        ]
