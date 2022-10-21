85e212eb9adad4556e4cef67bcdd1d42985eb564
unknown <natapon1996@gmail.com>
2022-10-21 16:07:42 +0700

    two-fer python solution

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,8 @@
+def two_fer(name = 'you'):
+    """
+    input:
+        - name (str) : default is 'you'
+    return:
+        - (str) One for name, one for me
+    """
+    return f'One for {name}, one for me.'
