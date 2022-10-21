40efaf769b8ae140cfa96ff77f7be02e6655c7c4
unknown <natapon1996@gmail.com>
2022-10-21 16:18:27 +0700

    leap-year python solution

---
+++ python/leap/leap.py
@@ -0,0 +1,7 @@
+# check leap year function
+def leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 != 0:
+            return False
+        return True
+    return False
