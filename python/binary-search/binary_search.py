60eb7c4b84b01729a9df354b90c16e6653f415df
Natapon <natapon1996@gmail.com>
2022-11-11 00:39:35 +0700

    binary-search python solution

--- python/binary-search/binary_search.py
+++ python/binary-search/binary_search.py
@@ -1,18 +1,13 @@
-index = 0 # global var to store ans index
 def find(search_list, value):
-    global index
     length = len(search_list)
     if length == 0:
         raise ValueError("value not in array")
     n = length//2 # index of the middle member
     # base case
     if value == search_list[n]:
-        index += n
-        ans, index = index, 0 # set global index to 0
-        return ans
+        return n
     elif value > search_list[n]:
-        index += n + 1
-        return find(search_list[n+1:], value)
+        return find(search_list[n+1:], value) + (n + 1)
     else:
         return find(search_list[:n], value)
         
